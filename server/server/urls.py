24b5a4911c2c28b494da80444c88657489aedd20
VivekBits2210 <viveknayak2210@gmail.com>
2021-07-14 09:39:29 +0530

    Moved server code to new repo

--- server/server/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("gre_chrome/", include("gre_chrome.urls")),
-    path("admin/", admin.site.urls),
-]
